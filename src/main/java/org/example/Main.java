3ee7c2eaab4bf1d403996da4021aed2363ac055a
aryanpasricha <aryan.pasricha@credable.in>
2022-03-02 11:02:38 +0530

    added Webdriver Manager, removed chrome and geko driver

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,12 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,7 +14,8 @@ public class Main {
 //        FirefoxOptions options=new FirefoxOptions();
 //        options.setBinary("/usr/bin/firefox");
 
-        WebDriver driver=chromeDriver();
+        //can call any function here
+        WebDriver driver=firefoxDriver();
         driver.get("http://google.com");
 
 //        document.getElementsbyName("q")[0] //this is how we inspect on chrome
@@ -31,10 +29,12 @@ public class Main {
     }
 
     public static WebDriver firefoxDriver() {
+        WebDriverManager.firefoxdriver().setup();
         return new FirefoxDriver();
     }
 
     public static WebDriver chromeDriver() {
+        WebDriverManager.chromedriver().setup();
         return new ChromeDriver();
     }
 }
