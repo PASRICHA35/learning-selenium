6c482fbbd8e8cede51cc160e94c73c1d5f8b7be6
aryanpasricha <aryan.pasricha@credable.in>
2022-03-04 11:04:38 +0530

    added test for 2017 blog page too.

--- src/test/java/org/example/SeleniumWebsiteTest.java
+++ src/test/java/org/example/SeleniumWebsiteTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,8 +12,8 @@ import static org.junit.Assert.*;
 public class SeleniumWebsiteTest {
 
 
-    @org.junit.Test
-    public void test_selenium_dot_dev_blog(){
+    @Test
+    public void test_selenium_dot_dev_blog_2016_page(){
         WebDriverManager.firefoxdriver().setup();
 
         WebDriver driver=new FirefoxDriver();
@@ -31,6 +32,26 @@ public class SeleniumWebsiteTest {
         String title=driver.getTitle();
         assertEquals("Blog Posts - 2016 | Selenium",title);
 
+        driver.quit();
+    }
+
+    @Test
+    public void test_selenium_dot_dev_blog_2017_page(){
+        WebDriverManager.firefoxdriver().setup();
+
+        WebDriver driver=new FirefoxDriver();
+        driver.get("https://www.selenium.dev/");
+
+        WebElement blog=driver.findElement(By.linkText("Blog"));
+        blog.click();
+
+        WebElement link2017=driver.findElement(By.id("m-blog2017"));
+        link2017.click();
+
+        String title=driver.getTitle();
+        assertEquals("Blog Posts - 2017 | Selenium",title);
+
+        driver.quit();
 
     }
 }
